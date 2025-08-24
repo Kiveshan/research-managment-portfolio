32a49fb70fa63fc39e2a8f5b9c610e8cc6e66c39
Kiveshan Naidoo <102474568+Kiveshan@users.noreply.github.com>
2025-08-24 20:16:18 +0200

    Getting a file structure ready

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config({ path: "./src/.env" });
 const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("landing");
 });
 
 app.listen(PORT, () => {
